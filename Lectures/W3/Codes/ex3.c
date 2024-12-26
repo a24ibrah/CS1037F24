91bf70cc765a76c87ee417e83811ee4734c6e185
a24ibrah <a24ibrah@users.noreply.github.com>
2024-12-26 09:06:20 +0200

    Updates Dec26!

--- Lectures/W3/Codes/ex3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() {
-  int x = 6;
-  printf("Value of x is %d\n", x);
-  printf("Runtime memory address of x in Hex is %p\n", &x);
-  printf("Runtime memory address of x in decimal is %lu\n", &x);
-  printf("Value stored at address %lu is %d\n", &x, *(&x));
-  return 0;
-}
--- Lectures/W3/Codes/ex6.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int num;
-    int *ptr;
-    ptr = &num;     // address assignment, ptr is pointing num
-    *ptr = 2;       // num will have value 2 at runtime
-    }
